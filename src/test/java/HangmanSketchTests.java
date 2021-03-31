05bdb63ab5bec13485f5f6a8f97548aaee773ab8
Irina Markov <irina@aquilatech.com>
2021-03-30 22:27:26 -0500

    passed drawHangmanZeroMistakesLeftTest

--- src/test/java/HangmanSketchTests.java
+++ src/test/java/HangmanSketchTests.java
@@ -2,34 +2,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HangmanSketchTests {
-//    @Test
-//    public void drawHangmanZeroMistakesLeftTest(){
-//
-//        String expected = "     ________\n" +
-//                          "     |/     |\n" +
-//                          "     |      |\n" +
-//                          "     |     (_)\n"+
-//                          "     |     /|\\ \n"+
-//                          "     |      |\n" +
-//                          "     |     / \\\n"+
-//                          "     |\n"        +
-//                          " ____|____";
-//        String actual = HangmanSketch.getInstance().drawHangman(0);
-//        Assertions.assertEquals(expected, actual);
-//    }
-//    @Test
-//    public void drawHangmanOneMistakeLeftTest(){
-//
-//        String expected = "     ________\n" +
-//                "     |/     |\n" +
-//                "     |      |\n" +
-//                "     |     (_)\n"+
-//                "     |     /|\\ \n"+
-//                "     |      |\n" +
-//                "     |     / \n"+
-//                "     |\n"        +
-//                " ____|____";
-//        String actual = HangmanSketch.getInstance().drawHangman(1);
-//        Assertions.assertEquals(expected, actual);
-//    }
+    @Test
+    public void drawHangmanZeroMistakesLeftTest(){
+
+        String expected = "\n\n" +
+                "                                                                               __________\n" +
+                "                                                                              |X|//     |\n" +
+                "                                                                              |X|       |\n" +
+                "                                                                              |X|      (xx)\n"+
+                "                                                                              |X|      /|\\\n"+
+                "                                                                              |X|       |\n" +
+                "                                                                              |X|      / \\\n"+
+                "                                                                              |X|\n"        +
+                "                                                                        ZZZZZZZZZZZZZZZ\n\n\n";
+        String actual = HangmanSketch.getInstance().drawHangman(0);
+        Assertions.assertEquals(expected, actual);
+    }
 }
